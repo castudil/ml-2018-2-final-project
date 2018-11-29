93a7d42710a2c50ca2f13d01a16498e589e20cb2
César Astudillo <cesar.astudillo@gmail.com>
2018-11-29 16:24:25 -0300

    Update preprocesamiento.R

--- preprocesamiento.R
+++ preprocesamiento.R
@@ -4,9 +4,23 @@
 
 #guardo la URL en una variable
 
-URL<-"https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wpbc.data"
+URL<-"https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
+
+#veamos cual es el link
+URL
+
 
 #cargo el CSV. en particular este archivo no tiene encabezado, es decir, no tiene una linea inicial con los nombres de los atributos.
 
-X <- read.csv(URL,
+data <- read.csv(URL,
               header = FALSE)
+data
+dim(data)
+
+
+plot(data) # no es capaz de procesar la informacion porque son muchas dimensiones
+str(data) # resumen de atributos, dice algunos valores y los rangos
+data$V2
+x<-data[-c(1,2),]
+str(x)
+dim(x)
